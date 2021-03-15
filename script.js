22abe178cec528258b1d092b661297c5d4d07dd6
raynaud <raynaud.cornille@gmail.com>
2021-03-15 11:27:49 +0100

    Landing page
    Landing page almost done, only needs some extra javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(()=>{
+    $(window).scroll(()=>{
+        if(this.scrollY > 20)
+        {
+            $('.navbar').addClass("sticky");
+        }
+        else  
+        {
+            $('.navbar').removeClass("sticky");
+        }
+    }) 
+});
