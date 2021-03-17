7fede6246cf2ac8f5cf5b6b4ac6ae9170237b17c
raynaud <raynaud.cornille@gmail.com>
2021-03-17 18:02:29 +0100

    Final
    Final design

--- script.js
+++ script.js
@@ -9,7 +9,34 @@ $(document).ready(()=>{
         {
             $('.navbar').removeClass("sticky");
         }
+        if(this.scrollY > 500)
+        {
+            $('.scroll-up-btn').addClass("show");
+        }
+        else  
+        {
+            $('.scroll-up-btn').removeClass("show");
+        }
     });
+
+    $('.scroll-up-btn').click(() => 
+    {
+        $('html').animate({scrollTop: 0});
+    });
+
+    var typed = new Typed (".typing", {
+        strings: ["Designer", "Developer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed (".typing-2", {
+        strings: ["Designer", "Developer", "Student"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
     $('.menu-btn').click(()=>
     {
         $('.navbar .menu').toggleClass("active");
