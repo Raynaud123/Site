de22335bff424c8698ccd330af0d399b337cf6fe
raynaud <raynaud.cornille@gmail.com>
2021-03-17 15:40:49 +0100

    Projects section
    Previous Projects section finished

--- script.js
+++ script.js
@@ -15,4 +15,26 @@ $(document).ready(()=>{
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+    
+    //carreeeee
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        respoonsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
